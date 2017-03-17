b25eb9facd7368acdc5fe81f27d9dd46714a3fcf
rr2d2 <rodriguezomar91@gmail.com>
2017-03-17 11:37:19 -0500

    Init stock beer ticker

--- js/beercontrol.jsx
+++ js/beercontrol.jsx
@@ -19,6 +19,7 @@ var BeerSalesControl = React.createClass({
     render: function () {
         return (
             React.createElement('div', { className: "beerSalesControl" },
+                React.createElement(TickerControl, { data: this.state.data }),
                 React.createElement(PageHeaderControl),
                 React.createElement(PricingInformationControl, { data: this.state.data }),
                 React.createElement(PointOfSaleControl)
@@ -108,6 +109,55 @@ var PricingRowControl = React.createClass({
                 <td>{item.max}</td>
             </tr>
             );
+    }
+});
+
+/* TICKER CONTROL */
+var TickerControl = React.createClass({
+    render: function () {
+         // Builds the Rows
+        var itemNodes = this.props.data.map(function (item) {
+            return (
+                React.createElement(TikerBeerItemControl, { key: item.id }, item)
+              );
+        });
+
+        // Build Header
+        return (
+            <font style={{'fontSize': 23}} color='black'>
+               <marquee behavior='scroll' direction='left' scrollamount='10'>
+                  {itemNodes}
+               </marquee>
+            </font>
+         );
+    }
+});
+
+var TikerBeerItemControl = React.createClass({
+    render: function () {
+        var Glyphicon = ReactBootstrap.Glyphicon;
+        var item = this.props.children;
+
+        var icon = "";
+        var priceColor = "";
+         if(item.price > item.basePrice)
+         {
+            priceColor = "red";
+            icon = React.createElement(Glyphicon, { glyph: "arrow-up", style: {color: priceColor, margin: "0 5 0 0"}});
+         }
+         else
+         {
+            priceColor = "green";
+            icon = React.createElement(Glyphicon, { glyph: "arrow-down", style: { color: priceColor, margin: "0 5 0 0"}});
+         }
+               
+
+        // Build Row
+        return (
+            <span>
+               {item.name}<span style={{color: priceColor}}> %{item.percentChange}</span>{icon}  
+            </span>
+         );
     }
 });
 
--- js/tickercontrol.jsx
+++
@@ -1,9 +0,0 @@
-﻿var BeerSalesControl = React.createClass({
-   render: function () {
-      return (<div>Beer #1 $3.75 -0.937%</div>);
-   }
-});
-ReactDOM.render(
-    React.createElement(BeerSalesControl),
-    document.getElementById('tickerContent')
-);
